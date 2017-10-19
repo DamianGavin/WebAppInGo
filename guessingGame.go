1de11d33ac31e336db4e718fe6d97e0b6357be56
Damian Gavin <10007969@gmit.ie>
2017-10-19 13:36:38 +0100

    simpler version

--- guessingGame.go
+++ guessingGame.go
@@ -1,42 +1,15 @@
-//Damian Gavin 18/10/17
-//A web application in Go that responds with the text “Guessing game”.
-
 package main
 
 import (
 	"fmt"
-	//"io"
-	"log"
 	"net/http"
-	"strconv"
 )
 
-func main() {
-	http.HandleFunc("/", foo)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
-	http.ListenAndServe(":8080", nil)
+func server(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Guessing Game")
 }
 
-func foo(res http.ResponseWriter, req *http.Request) {
-
-	cookie, err := req.Cookie("my-cookie")
-
-	if err == http.ErrNoCookie {
-		cookie = &http.Cookie{
-			Name:  "my-cookie",
-			Value: "0",
-		}
-	}
-
-	count, err := strconv.Atoi(cookie.Value)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	count++
-	cookie.Value = strconv.Itoa(count)
-
-	http.SetCookie(res, cookie)
-
-	//io.WriteString(res, cookie.Value)
-	fmt.Println("Guessing Game")
+func main(){
+	http.HandleFunc("/", server)
+	http.ListenAndServe(":8080", nil)
 }
