bfac1846ffefbfc40e8b1d8a52a378d1dc4cb6c0
Damian Gavin <10007969@gmit.ie>
2017-10-19 13:10:00 +0100

    test

---
+++ guessingGame.go
@@ -0,0 +1,42 @@
+//Damian Gavin 18/10/17
+//A web application in Go that responds with the text “Guessing game”.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+
+	cookie, err := req.Cookie("my-cookie")
+
+	if err == http.ErrNoCookie {
+		cookie = &http.Cookie{
+			Name:  "my-cookie",
+			Value: "0",
+		}
+	}
+
+	count, err := strconv.Atoi(cookie.Value)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	count++
+	cookie.Value = strconv.Itoa(count)
+
+	http.SetCookie(res, cookie)
+
+	//io.WriteString(res, cookie.Value)
+	fmt.Println("Guessing Game")
+}
